5fd5ec51fb4571069156cf2ff6fee9a5df13ed8a
spixa <spixa@github.com>
2023-06-20 16:50:00 +0330

    Formatted

--- src/server.rs
+++ src/server.rs
@@ -1,35 +1,46 @@
-use crate::{types::{Client, MSG_SIZE}, util::sleep};
+use crate::{
+    types::{Client, MSG_SIZE},
+    util::sleep,
+};
 use stcp::{bincode, AesPacket, StcpServer};
-use std::{io::{ErrorKind, Read, Write}, sync::mpsc::{self}, thread};
+use std::{
+    io::{ErrorKind, Read, Write},
+    sync::mpsc::{self},
+    thread,
+};
 
 pub fn server() {
-
     println!("generating keypairs...");
     let server = StcpServer::bind("0.0.0.0:37549").unwrap();
     println!("server running on port 37549");
 
-    server.listener.set_nonblocking(true).expect("failed to initialize non-blocking");
+    server
+        .listener
+        .set_nonblocking(true)
+        .expect("failed to initialize non-blocking");
 
-    let mut clients : Vec<Client>= vec![];
-    
+    let mut clients: Vec<Client> = vec![];
+
     let (tx, rx) = mpsc::channel::<String>();
 
     loop {
         if let Ok((mut socket, addr)) = server.listener.accept() {
             println!("{} connected", addr);
-            
+
             let aes = server.kex_with_stream(&mut socket);
             println!("KEX completed with {}", socket.peer_addr().unwrap());
 
             let _tx = tx.clone();
             let mut _aes = aes.clone();
 
-            clients.push(Client(socket.try_clone().expect("failed to clone client"), aes));
+            clients.push(Client(
+                socket.try_clone().expect("failed to clone client"),
+                aes,
+            ));
 
             thread::spawn(move || loop {
                 //let mut buff = vec![0; MSG_SIZE];
                 let mut buff = [0_u8; MSG_SIZE];
-
 
                 match socket.read(&mut buff) {
                     Ok(size) => {
@@ -42,7 +53,7 @@ pub fn server() {
                             Err(_) => {
                                 println!("closing connection with: {}", addr);
                                 break;
-                            }   
+                            }
                         }
 
                         let packet = packet.unwrap();
@@ -50,10 +61,10 @@ pub fn server() {
                         let decrypted_data = packet.decrypt(&mut _aes);
 
                         let msg = String::from_utf8(decrypted_data).expect("Invalid message");
-                    
+
                         println!("{}: {:?}", addr, msg);
                         _tx.send(msg).expect("Failed to send message");
-                    },
+                    }
 
                     Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
 
@@ -68,12 +79,15 @@ pub fn server() {
         }
 
         if let Ok(msg) = rx.try_recv() {
-            clients = clients.into_iter().filter_map(|mut client| {
-                let client_msg = msg.clone().into_bytes();
-                let reply = AesPacket::encrypt_to_bytes(&mut client.1, client_msg.to_vec());
-                
-                client.0.write(&reply).map(|_| client).ok()
-            }).collect::<Vec<_>>();
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let client_msg = msg.clone().into_bytes();
+                    let reply = AesPacket::encrypt_to_bytes(&mut client.1, client_msg.to_vec());
+
+                    client.0.write(&reply).map(|_| client).ok()
+                })
+                .collect::<Vec<_>>();
         }
 
         sleep();
--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,15 @@
-use std::{thread, io::{Write, self}};
+use std::{
+    io::{self, Write},
+    thread,
+};
 
 pub fn sleep() {
     thread::sleep(::std::time::Duration::from_millis(50));
 }
 
-
 pub fn ask(prompt: &str) -> String {
     print!("{}", prompt);
-    std::io::stdout()
-        .flush()
-        .unwrap();
+    std::io::stdout().flush().unwrap();
 
     let mut answer = String::new();
     io::stdin()
