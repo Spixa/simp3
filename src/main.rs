4ba2c7662ac66577a7946801ffa5939c6882ab18
spixa <spixa@github.com>
2023-06-21 20:52:52 +0330

    Start to add packet parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // for now i will be making an unencrypted one
 // then we will be integrating stcp into this
 mod client;
+mod net;
 mod server;
 mod types;
 mod util;
@@ -8,6 +9,7 @@ mod util;
 use crate::{client::client, server::server, util::ask};
 
 fn main() {
+
     let answer = ask("server or client: ");
 
     match answer.as_str() {
