d219f12a33450cc4bc2eaa5a8b0360457f99e9ba
spixa <spixa@github.com>
2023-06-23 19:39:43 +0330

    Added commands and leave message

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-// for now i will be making an unencrypted one
-// then we will be integrating stcp into this
 mod client;
 mod net;
 mod server;
 mod types;
 mod util;
 
-use crate::{client::client, server::server, util::ask};
+use colored::Colorize;
+
+use crate::{util::ask};
 
 fn main() {
     let answer = ask("server or client: ");
 
-    match answer.as_str() {
-        "server" | "s" => server(),
-        "client" | "c" => client(),
-        &_ => println!("invalid answer: type server or client"),
+    match answer.to_lowercase().as_str() {
+        "server" | "s" => server::do_server(),
+        "client" | "c" | "" => client::do_client(),
+        &_ => println!("{}", "invalid answer: type server or client".red()),
     }
 }
